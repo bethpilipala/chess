276f63dc70d4385dd2c100a4ce3720d41d88dc56
Beth Perkins <beth@perkcan.net>
2026-09-15 23:34:05 -0600

    Start implementation

--- shared/src/main/java/chess/ChessBoard.java
+++ shared/src/main/java/chess/ChessBoard.java
@@ -8,6 +8,8 @@ package chess;
  */
 public class ChessBoard {
 
+    private ChessPosition[][] board = new ChessPosition[8][8];
+
     public ChessBoard() {
         
     }
@@ -19,7 +21,7 @@ public class ChessBoard {
      * @param piece    the piece to add
      */
     public void addPiece(ChessPosition position, ChessPiece piece) {
-        throw new RuntimeException("Not implemented");
+        return board[position.getRow()-1][position.getColumn()-1] = piece;
     }
 
     /**
