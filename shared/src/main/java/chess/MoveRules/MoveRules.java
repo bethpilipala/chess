83d6723c49e6d38d3a60adcf05a83c36edf06e7c
Beth Perkins <beth@perkcan.net>
2026-09-21 23:03:51 -0600

    Check if in bounds

--- shared/src/main/java/chess/MoveRules/MoveRules.java
+++ shared/src/main/java/chess/MoveRules/MoveRules.java
@@ -8,4 +8,11 @@ public abstract class MoveRules {
 
     public abstract Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition position);
 
+    protected boolean inBounds(int row, int column) {
+        if (row >= 1 && row <= 8 && column >= 1 && column <= 8)
+            return true;
+        else
+            return false;
+    }
+
 }
