38d9576b8e9a605a6bb259c853c0a5a6f6092c34
Beth Perkins <beth@perkcan.net>
2026-09-22 01:10:11 -0600

    Update PawnMoveRules.java

--- shared/src/main/java/chess/MoveRules/PawnMoveRules.java
+++ shared/src/main/java/chess/MoveRules/PawnMoveRules.java
@@ -11,7 +11,13 @@ public class PawnMoveRules extends MoveRules {
 
     @Override
     public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition position) {
-        return new ArrayList<>();
-    }
+        Collection<ChessMove> moves = new ArrayList<>();
 
+        addSingleMoves(board, position, 1, 0, moves); //up
+        addSingleMoves(board, position, 2, 0, moves); //up 2
+        addSingleMoves(board, position, 1, -1, moves); //up and left
+        addSingleMoves(board, position, 1, 1, moves); //up and right
+
+        return moves;
+    }
 }
